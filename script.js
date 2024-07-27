2851b8091abd148e69f7c877d3303deac4d1893d
harrisonBirkner <harrison.birkner@gmail.com>
2024-07-27 12:01:36 -0500

    restart added. Bug: cells don't unhide after rstrt

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const gameBoard = document.getElementById('game-board');
+let gameBoard = document.getElementById('game-board');
 const rows = 10;
 const cols = 10;
 const minesCount = 20;
 let cells = [];
 let mineIndices = new Set();
+const restartBtn = document.getElementById('restart');
+restartBtn.addEventListener('click', () => restartGame());
 
 //init game
 function initGame() {
@@ -11,12 +13,12 @@ function initGame() {
     
     // Create a unique set of mine indices
     while (mineIndices.size < minesCount) {
-        const randomIndex = Math.floor(Math.random() * (rows * cols));
+        let randomIndex = Math.floor(Math.random() * (rows * cols));
         mineIndices.add(randomIndex);
     }
 
     for (let i = 0; i < rows * cols; i++) {
-        const cell = document.createElement('div');
+        let cell = document.createElement('div');
         cell.classList.add('cell', 'hidden');
 
         // Check if the current index is a mine
@@ -34,13 +36,35 @@ function initGame() {
 
 function handleCellClick(i) {
     //game logic for handling cell clicks
+    console.log('cell clicked', i);
     cells[i].classList.remove('hidden');
+    console.log('after hidden class removal')
     if (cells[i].classList.contains('mine')) {
-        cells.forEach(cell => {
-            cell.classList.remove('hidden');
-        });
-        gameBoard.style.pointerEvents = 'none';
+        console.log('MINE CLICKED');
+        gameOver();
     }
+}
+
+function gameOver() {
+    cells.forEach(cell => {
+        cell.classList.remove('hidden');
+    });
+    gameBoard.style.pointerEvents = 'none';
+    let gameOverDiv = document.createElement('div');
+    gameOverDiv.id = 'game-over';
+    let gameOverMsg = document.createElement('h2');
+    gameOverMsg.style.color = 'red';
+    gameOverMsg.textContent = 'Game Over';
+    gameOverDiv.appendChild(gameOverMsg);
+    document.body.appendChild(gameOverDiv);
+}
+
+function restartGame() {
+    gameBoard.textContent = '';
+    gameBoard.style.pointerEvents = '';
+    mineIndices.clear();
+    document.body.removeChild(document.getElementById('game-over'));
+    initGame();
 }
 
 initGame();
