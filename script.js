3cc0c411b3129d517a70b4e53ee21db7c56ea5df
harrisonBirkner <harrison.birkner@gmail.com>
2025-02-19 19:30:11 -0600

    reworked how cells are numbered. Numbers are made visible as they are clicked and blanks are expanded correctly

--- script.js
+++ script.js
@@ -34,7 +34,11 @@ function initGame() {
         cells[i].addEventListener('click', () => handleCellClick(i));
     }
 
-    //additional logic for mines and game state mgmt here
+    for (let i = 0; i < cells.length; i++) {
+        if (!cells[i].classList.contains('mine')) {
+            checkForSurroundingCells(i);
+        }
+    }
 }
 
 function handleCellClick(i) {
@@ -47,6 +51,7 @@ function handleCellClick(i) {
         gameOver();
     }
     else {
+        cells[i].children[0].style.visibility = 'visible';
         checkAndExpandSurroundingCells(i);
     }
 }
@@ -76,9 +81,21 @@ function restartGame() {
     });
 
     let minesToReset = document.querySelectorAll('.mine');
-    cellsToHide.forEach(cell => {
+    minesToReset.forEach(cell => {
         cell.classList.remove('mine');
     });
+
+    let numberedSquaresToReset = document.querySelectorAll('.numbered');
+    numberedSquaresToReset.forEach(cell => {
+        cell.classList.remove('numbered');
+    })
+
+    let numbersToDelete = gameBoard.querySelectorAll('b');
+    numbersToDelete.forEach(number => {
+        number.remove();
+    });
+    //nodeList.forEach(node => node.remove());
+    //TODO: delete <b> tags
 
     initGame();
 }
@@ -98,7 +115,7 @@ function pauseGame() {
 }
 
 function checkAndExpandSurroundingCells(i) {
-    if (i == null) {
+    if ((i == null) || (cells[i].classList.contains('mine'))) {
         return;
     }
     surroundingCells = new Array(8).fill(null); //any given square will have a max of 8 squares surrounding it
@@ -115,7 +132,7 @@ function checkAndExpandSurroundingCells(i) {
         (i >= 71 && i <= 79) || 
         (i >= 81 && i <= 89) || 
         (i >= 91 && i <= 99)) {
-            determineIfSurroundingCellIsMine(i - 11, 0);
+            determineIfSurroundingCellIsNumbered(i - 11, 0, i);
     }
     //top middle
     if ((i >= 10 && i <= 19) || 
@@ -127,7 +144,7 @@ function checkAndExpandSurroundingCells(i) {
         (i >= 70 && i <= 79) || 
         (i >= 80 && i <= 89) || 
         (i >= 90 && i <= 99)) {
-            determineIfSurroundingCellIsMine(i - 10, 1);
+            determineIfSurroundingCellIsNumbered(i - 10, 1, i);
     }
     //top right 
     if ((i >= 10 && i <= 18) || 
@@ -139,7 +156,7 @@ function checkAndExpandSurroundingCells(i) {
         (i >= 70 && i <= 78) || 
         (i >= 80 && i <= 88) || 
         (i >= 90 && i <= 98)) {
-            determineIfSurroundingCellIsMine(i - 9, 2);
+            determineIfSurroundingCellIsNumbered(i - 9, 2, i);
     }
     //middle right
     if ((i >= 0 && i <= 8) ||
@@ -152,7 +169,7 @@ function checkAndExpandSurroundingCells(i) {
         (i >= 70 && i <= 78) || 
         (i >= 80 && i <= 88) || 
         (i >= 90 && i <= 98)) {
-            determineIfSurroundingCellIsMine(i + 1, 3);
+            determineIfSurroundingCellIsNumbered(i + 1, 3, i);
     }
     //bottom right
     if ((i >= 0 && i <= 8) ||
@@ -164,7 +181,7 @@ function checkAndExpandSurroundingCells(i) {
         (i >= 60 && i <= 68) || 
         (i >= 70 && i <= 78) || 
         (i >= 80 && i <= 88)) {
-            determineIfSurroundingCellIsMine(i + 11, 4);
+            determineIfSurroundingCellIsNumbered(i + 11, 4, i);
     }
     //bottom middle
     if ((i >= 0 && i <= 9) ||
@@ -176,7 +193,7 @@ function checkAndExpandSurroundingCells(i) {
         (i >= 60 && i <= 69) || 
         (i >= 70 && i <= 79) || 
         (i >= 80 && i <= 89)) {
-            determineIfSurroundingCellIsMine(i + 10, 5);
+            determineIfSurroundingCellIsNumbered(i + 10, 5, i);
     }
     //bottom left
     if ((i >= 1 && i <= 9) ||
@@ -188,7 +205,7 @@ function checkAndExpandSurroundingCells(i) {
         (i >= 61 && i <= 69) || 
         (i >= 71 && i <= 79) || 
         (i >= 81 && i <= 89)) {
-            determineIfSurroundingCellIsMine(i + 9, 6);
+            determineIfSurroundingCellIsNumbered(i + 9, 6, i);
     }
     //middle left
     if ((i >= 1 && i <= 9) ||
@@ -201,26 +218,151 @@ function checkAndExpandSurroundingCells(i) {
         (i >= 71 && i <= 79) || 
         (i >= 81 && i <= 89) || 
         (i >= 91 && i <= 99)) {
-            determineIfSurroundingCellIsMine(i - 1, 7);
+            determineIfSurroundingCellIsNumbered(i - 1, 7, i);
+    }
+
+    //
+    
+    surroundingCells.forEach(checkAndExpandSurroundingCells);
+}
+
+function determineIfSurroundingCellIsNumbered(indexOfCurrentSurroundingCell, indexInSurroundingCellArray, indexofCurrentCell) {
+    if ((cells[indexOfCurrentSurroundingCell].classList.contains('hidden'))
+        && (cells[indexofCurrentCell].classList.contains('blank'))) {
+        //TODO: make numbers visible after reveal
+        cells[indexOfCurrentSurroundingCell].classList.remove('hidden');
+        if (cells[indexOfCurrentSurroundingCell].classList.contains('numbered')) {
+            cells[indexOfCurrentSurroundingCell].children[0].style.visibility = 'visible';
+        }  
+        else if (cells[indexOfCurrentSurroundingCell].classList.contains('blank')) {
+            surroundingCells[indexInSurroundingCellArray] = indexOfCurrentSurroundingCell;
+        }      
+    }
+}
+
+function checkForSurroundingCells(i) {
+    console.log('looking at cell ' + i);
+    numOfSurroundingMines = 0;
+
+    //top left
+    if ((i >= 11 && i <= 19) || 
+        (i >= 21 && i <= 29) || 
+        (i >= 31 && i <= 39) || 
+        (i >= 41 && i <= 49) || 
+        (i >= 51 && i <= 59) || 
+        (i >= 61 && i <= 69) || 
+        (i >= 71 && i <= 79) || 
+        (i >= 81 && i <= 89) || 
+        (i >= 91 && i <= 99)) {
+            console.log('cell ' + i + ' has a top left cell');
+            determineCellType(i - 11);
+    }
+    //top middle
+    if ((i >= 10 && i <= 19) || 
+        (i >= 20 && i <= 29) || 
+        (i >= 30 && i <= 39) || 
+        (i >= 40 && i <= 49) || 
+        (i >= 50 && i <= 59) || 
+        (i >= 60 && i <= 69) || 
+        (i >= 70 && i <= 79) || 
+        (i >= 80 && i <= 89) || 
+        (i >= 90 && i <= 99)) {
+            console.log('cell ' + i + ' has a top middle cell');
+            determineCellType(i - 10);
+    }
+    //top right 
+    if ((i >= 10 && i <= 18) || 
+        (i >= 20 && i <= 28) || 
+        (i >= 30 && i <= 38) || 
+        (i >= 40 && i <= 48) || 
+        (i >= 50 && i <= 58) || 
+        (i >= 60 && i <= 68) || 
+        (i >= 70 && i <= 78) || 
+        (i >= 80 && i <= 88) || 
+        (i >= 90 && i <= 98)) {
+            console.log('cell ' + i + ' has a top right cell');
+            determineCellType(i - 9);
+    }
+    //middle right
+    if ((i >= 0 && i <= 8) ||
+        (i >= 10 && i <= 18) || 
+        (i >= 20 && i <= 28) || 
+        (i >= 30 && i <= 38) || 
+        (i >= 40 && i <= 48) || 
+        (i >= 50 && i <= 58) || 
+        (i >= 60 && i <= 68) || 
+        (i >= 70 && i <= 78) || 
+        (i >= 80 && i <= 88) || 
+        (i >= 90 && i <= 98)) {
+            console.log('cell ' + i + ' has a middle right cell');
+            determineCellType(i + 1);
+    }
+    //bottom right
+    if ((i >= 0 && i <= 8) ||
+        (i >= 10 && i <= 18) || 
+        (i >= 20 && i <= 28) || 
+        (i >= 30 && i <= 38) || 
+        (i >= 40 && i <= 48) || 
+        (i >= 50 && i <= 58) || 
+        (i >= 60 && i <= 68) || 
+        (i >= 70 && i <= 78) || 
+        (i >= 80 && i <= 88)) {
+            console.log('cell ' + i + ' has a bottom right cell');
+            determineCellType(i + 11);
+    }
+    //bottom middle
+    if ((i >= 0 && i <= 9) ||
+        (i >= 10 && i <= 19) || 
+        (i >= 20 && i <= 29) || 
+        (i >= 30 && i <= 39) || 
+        (i >= 40 && i <= 49) || 
+        (i >= 50 && i <= 59) || 
+        (i >= 60 && i <= 69) || 
+        (i >= 70 && i <= 79) || 
+        (i >= 80 && i <= 89)) {
+            console.log('cell ' + i + ' has a bottom middle cell');
+            determineCellType(i + 10);
+    }
+    //bottom left
+    if ((i >= 1 && i <= 9) ||
+        (i >= 11 && i <= 19) || 
+        (i >= 21 && i <= 29) || 
+        (i >= 31 && i <= 39) || 
+        (i >= 41 && i <= 49) || 
+        (i >= 51 && i <= 59) || 
+        (i >= 61 && i <= 69) || 
+        (i >= 71 && i <= 79) || 
+        (i >= 81 && i <= 89)) {
+            console.log('cell ' + i + ' has a bottom left cell');
+            determineCellType(i + 9);
+    }
+    //middle left
+    if ((i >= 1 && i <= 9) ||
+        (i >= 11 && i <= 19) || 
+        (i >= 21 && i <= 29) || 
+        (i >= 31 && i <= 39) || 
+        (i >= 41 && i <= 49) || 
+        (i >= 51 && i <= 59) || 
+        (i >= 61 && i <= 69) || 
+        (i >= 71 && i <= 79) || 
+        (i >= 81 && i <= 89) || 
+        (i >= 91 && i <= 99)) {
+            console.log('cell ' + i + ' has a middle left cell');
+            determineCellType(i - 1);
     }
 
     if (numOfSurroundingMines > 0) {
         cells[i].innerHTML = '<b>' + numOfSurroundingMines + '</b>';
+        cells[i].classList.add('numbered');
     }
-    console.log(surroundingCells);
-    surroundingCells.forEach(checkAndExpandSurroundingCells);
+    else {
+        cells[i].classList.add('blank');
+    }
 }
 
-function determineIfSurroundingCellIsMine(indexOfCurrentSurroundingCell, indexInSurroundingCellArray) {
-    if (cells[indexOfCurrentSurroundingCell].classList.contains('hidden')) {
-        if (cells[indexOfCurrentSurroundingCell].classList.contains('mine')) {
-            numOfSurroundingMines++;
-        }  
-        else {
-            surroundingCells[indexInSurroundingCellArray] = indexOfCurrentSurroundingCell;
-            cells[surroundingCells[indexInSurroundingCellArray]].classList.remove('hidden');
-            console.log('after hidden class removal');
-        }
+function determineCellType(indexOfCurrentSurroundingCell) {
+    if (cells[indexOfCurrentSurroundingCell].classList.contains('mine')) {
+        numOfSurroundingMines++;
     }
 }
 
